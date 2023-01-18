f4e414ccd23d373b88798150c9819163efc35ad7
jamesjoel <jamesjoelj@gmail.com>
2023-01-18 17:28:45 +0530

    node -3

---
+++ javascript/array.js
@@ -0,0 +1,3 @@
+let data = ["red", "green", "blue", 500, true, 78.52];
+
+console.log(data[3]+data[5]); 
---
+++ javascript/array2.js
@@ -0,0 +1,16 @@
+/*
+let a = 5;
+let b = a;
+
+a = 10;
+
+console.log(a);
+console.log(b); */
+
+let data = ["red", "green", "blue"];
+let data2 = data;
+
+data[0]="rohit-----*****";
+
+console.log(data);
+console.log(data2);
