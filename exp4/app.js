4dce65ab2ab06ec5b455679c4bec0c2e32bb5301
jamesjoel <jamesjoelj@gmail.com>
2023-02-02 17:13:10 +0530

    foreach in es-6

--- exp4/app.js
+++ exp4/app.js
@@ -19,8 +19,90 @@ app.get("/", (req, res)=>{
 app.get("/contact", (req, res)=>{
     res.render("contact");
 });
+app.get("/product", (req, res)=>{
+
+    let data = ["Mobiles", "Home Appliance", "Fashion Men", "Fashion Women", "Kids"];
+
+    let product = [
+        {
+            title : "I-Phone",
+            price : 54000.00,
+            img : "2.jpg"
+        },
+        {
+            title : "Samsung",
+            price : 27000.00,
+            img : "3.jpg"
+        },
+        {
+            title : "HTC",
+            price : 24500.00,
+            img : "4.jpg"
+        },
+        {
+            title : "Mi",
+            price : 31000.00,
+            img : "5.jpg"
+        },
+        {
+            title : "Vivo",
+            price : 18500.00,
+            img : "6.jpg"
+        }
+
+        
+    ]
+
+    let obj = {product, data};
+
+
+    res.render("product", obj);
+});
+
+
+app.get("/student", (req, res)=>{
+    let color = ["red", "green", "blue", "yellow", "pink"];
+    let user = [
+        {
+            name : "rohit",
+            age : 25,
+            city : "inodre",
+            fee : 5000
+        },
+        {
+            name : "jaya",
+            age : 21,
+            city : "mumbai",
+            fee : 5500
+        },
+        {
+            name : "amar",
+            age : 27,
+            city : "inodre",
+            fee : 7000
+        },
+        
+        {
+            name : "gaurav",
+            age : 24,
+            city : "pune",
+            fee : 8000
+        },
+        {
+            name : "nidhi",
+            age : 20,
+            city : "pune",
+            fee : 4500
+        }
+    ]
+    let obj = {color, user};
+    res.render("student", obj);
+});
+
+
 app.get("/help", (req, res)=>{
-    res.render("help");
+    let obj = {age : 33, city1 : "indore", city2 : "mumbai"}
+    res.render("help", obj);
 });
 
 
